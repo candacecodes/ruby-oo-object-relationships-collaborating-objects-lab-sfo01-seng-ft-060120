68e9aa15cab80e3f77450f2c44b2c28a006bbaea
eager-class-5814 <candacehchoe@gmail.com>
2020-06-08 19:46:09 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,15 @@
+class MP3Importer 
+  attr_accessor :path 
+  
+  def initialize(path)
+    @path = path 
+  end 
+  
+  def files 
+    @path << Dir[MP3Importer.new('./spec/fixtures').import]
+  end 
+  
+  def import 
+  end 
+  
+end 
---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song 
+  attr_accessor :name, :artist 
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.new_by_filename(file)
+    parsed_file = file.split(" - ")
+    Song.new.name = parsed_file[0]
+    Song.new.artist = parsed_file[1]
+  end 
+  
+  def artist_name=(name) #getter 
+    if Artist.all.include?(name)
+      self.artist = name 
+    else 
+      Artist.new(name)
+      self.artist = name 
+    end 
+  end 
+end 
