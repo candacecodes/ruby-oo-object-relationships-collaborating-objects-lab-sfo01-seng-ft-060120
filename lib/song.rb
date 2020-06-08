ae57231b427ae7bb4dd73c797311af03977ebde0
eager-class-5814 <candacehchoe@gmail.com>
2020-06-08 19:58:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,12 +12,13 @@ class Song
   end 
   
   def self.new_by_filename(file)
-    array = file.split (' - ') 
+
+    array = file.split (' - ')
     song = Song.new(array[1])
-    
+
     song.artist = Artist.find_or_create_by_name(array[0])
-    
-  end 
+    song
+  end
   
   def artist_name=(name) #getter 
     Artist.all.each do |artist| 
